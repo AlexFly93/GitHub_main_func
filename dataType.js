5f0f512fea0692d637956b183bc23c7c8f5b96cd
Alexandr <muhabon93@gmail.com>
2024-03-13 13:41:18 +0200

    Add JS data types

---
+++ dataType.js
@@ -0,0 +1,24 @@
+
+
+//Числа (Numbers): 
+console.log(5+3);
+
+//Строки (Strings):  
+console.log('Hello' + ' World')
+
+//Булевыe значения (Boolean): 
+console.log(5===5);
+console.log(5!=5);
+
+//Массивы (Arrays): 
+const arr = [1, 2, 3, 4]
+console.log(arr);
+
+//Объекты (Objects): 
+const obj = { name: 'John', age: 30 }
+console.log(obj);
+
+Undefined: Пример: let x;
+
+Null: Пример: let y = null;
+
